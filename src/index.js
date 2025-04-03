9e1e15f2cb63498101d8763ac3695700697018ef
Aliabdulghani <20dcd20@gmail.com>
2025-04-03 21:58:13 +0300

    Edit index.js from browserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // تأكد من أن مسار الاستيراد صحيح
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { HashRouter } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>,
+  </HashRouter>,
   document.getElementById("root")
 );
